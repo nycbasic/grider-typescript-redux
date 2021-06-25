45ddd6b6ced0d37fa33fea96f3b808b6d7798367
nycbasic <samuel.poon@baruchmail.cuny.edu>
2021-06-24 20:21:00 -0400

    event types and calling an action creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,22 @@
+import React, { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import { actionCreators } from '../state';
+
 const RepositoriesList: React.FC = () => {
+	const [term, setTerm] = useState('');
+    const dispatch = useDispatch();
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        dispatch(actionCreators.SearchRepositories(term))
+    }
+
 	return (
 		<div>
-			<input />
-			<button>Search</button>
+			<form onSubmit={onSubmit}>
+				<input value={term} onChange={(e) => setTerm(e.target.value)} />
+				<button>Search</button>
+			</form>
 		</div>
 	);
 };
